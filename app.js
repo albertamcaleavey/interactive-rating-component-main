423bf2c112e3901fa25b5d098826fe8b4b097ee9
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-06-13 13:33:25 -0400

    create cached element references for rating and thank you states

---
+++ app.js
@@ -0,0 +1,9 @@
+const thankYouState = document.querySelector(".thank-you-container")
+console.log(thankYouState)
+
+const ratingState = document.querySelector(".rating-container")
+console.log(ratingState)
+
+// thankYouState.style.display = 'none'
+
+
